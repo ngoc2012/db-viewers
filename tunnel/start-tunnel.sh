8fb1daab019de96a9697ab0950340e6acd90249f
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2025-07-11 17:50:00 +0700

    clean

--- tunnel/start-tunnel.sh
+++ tunnel/start-tunnel.sh
@@ -2,9 +2,6 @@
 
 chmod 600 /root/.ssh/id_rsa.pub
 
-# Example: forward local port 3307 to db.example.com:3306 via ssh.example.com
-    # -o StrictHostKeyChecking=yes -N \
-
 # echo "$SSH_IDFILE" "$SSH_DOMAIN" "$SSH_USER" "$SSH_DOMAIN"
 ssh -i "$SSH_IDFILE" \
     -o StrictHostKeyChecking=no -N \
