038e6d05527289e61fd09c4d6a12f81d8388009b
Nguyen Minh Ngoc <ngocnm@comacpro.vn>
2025-07-11 15:52:19 +0700

    start tunnel

---
+++ tunnel/start-tunnel.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# Example: forward local port 3307 to db.example.com:3306 via ssh.example.com
+ssh -i "$SSH_IDFILE" \
+    -o StrictHostKeyChecking=yes -N \
+    -L 0.0.0.0:5433:"$SSH_DOMAIN":5433 \
+    -L 0.0.0.0:3308:"$SSH_DOMAIN":3308 \
+    "$SSH_USER"@"$SSH_DOMAIN"
